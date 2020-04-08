b94454d8a2eca086232c29f47788d92396a04617
Daniel Cordero <catalyst@0xdc.io>
2020-04-08 13:08:24 +0000

    embedded: forcibly unmask packages to be installed
    Instead of managing portage_confdir manually, let autounmask and etc-update
    forcibly make changes to keywords and USE flags.
    
    If portage fails to find a unmasked solution automatically, fail the build.

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -9,7 +9,18 @@ export clst_root_path="/"
 setup_pkgmgr
 
 echo "Installing dependencies into ${clst_root_path}..."
-run_merge --update --onlydeps "${clst_embedded_packages}"
+export LC_CTYPE=C.utf8
+timeout=4
+# call emerge directly as run_merge has '|| exit 1' which we don't desire
+until emerge --quiet --update --onlydeps \
+		--usepkg --buildpkg --binpkg-respect-use=y \
+		--autounmask --autounmask-write --keep-going --onlydeps-with-rdeps=n \
+		${clst_embedded_packages[@]}; do
+	test "$timeout" -gt 0 || exit 1
+	etc-update --quiet --automode -5
+	timeout=$(( $timeout - 1 ))
+done
+unset LC_CTYPE
 
 export clst_root_path="${DESTROOT}"
 export INSTALL_MASK="${clst_install_mask}"
