5f313dc125a038041ae2888c8a8dcbd32fa0f399
Daniel Cordero <catalyst@0xdc.io>
2020-10-14 17:55:48 +0000

    embedded: allow portage to apply automatic unmasking
    Instead of managing dependant use flags and keywords for all sorts of
    stages, allow portage to use --autounmask-continue to apply any
    additional flags for a package.

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -7,17 +7,23 @@ export DESTROOT="${clst_root_path}"
 export clst_root_path="/"
 
 echo "Installing dependencies into ${clst_root_path}..."
-run_merge --update --onlydeps "${clst_embedded_packages}"
+LC_CTYPE=C.utf8 run_merge --update --onlydeps --onlydeps-with-rdeps=n \
+	--autounmask --autounmask-continue \
+	"${clst_embedded_packages}"
 
 export clst_root_path="${DESTROOT}"
 export INSTALL_MASK="${clst_install_mask}"
 
 echo "Installing base system into ${clst_root_path}..."
-run_merge --oneshot sys-apps/baselayout
+run_merge sys-apps/baselayout
 
 export USE="-* build systemd udev"
-run_merge --oneshot app-shells/bash sys-apps/systemd sys-libs/glibc
+for package in sys-libs/glibc app-shells/bash sys-apps/systemd; do
+	run_merge "${package}"
+done
 unset USE
 
 echo "Installing packages into ${clst_root_path}..."
-LC_CTYPE=C.utf8 run_merge --oneshot "${clst_embedded_packages}"
+for package in ${clst_embedded_packages}; do
+	LC_CTYPE=C.utf8 run_merge "${package}"
+done
