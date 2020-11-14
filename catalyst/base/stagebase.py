29baaae76b1367caed3045ca5e1a8c00eca25c32
Daniel Cordero <catalyst@0xdc.io>
2020-11-14 16:46:57 +0000

    patch remove-machineid

--- catalyst/base/stagebase.py
+++ catalyst/base/stagebase.py
@@ -435,6 +435,7 @@ class StageBase(TargetBase, ClearBase, GenBase):
 
     def set_cleanables(self):
         self.settings['cleanables'] = [
+            "/etc/machine-id",
             "/etc/resolv.conf",
             "/var/tmp/*",
             "/tmp/*",
