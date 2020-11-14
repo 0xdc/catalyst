e9924e4d3acc8760ec4b9d5511e88150fb17b3f1
Daniel Cordero <catalyst@0xdc.io>
2020-11-14 16:41:57 +0000

    embedded: install systemd and deps
    We use the embedded target to build smaller systemd stages with less
    packages inside. It's not as small as a regular embedded stage as we
    need additional dependencies and dynamic linking from glibc.
    
    This produces a stage that has the minimum to run systemd.

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -8,6 +8,13 @@ ROOT=/ run_merge --update --onlydeps "${clst_embedded_packages}"
 export ROOT="${clst_root_path}"
 mkdir -p "$ROOT"
 
+echo "Installing base system into ${clst_root_path}..."
+run_merge --oneshot sys-apps/baselayout
+
+export USE="-* build systemd udev"
+run_merge --oneshot app-shells/bash sys-apps/systemd sys-libs/glibc
+unset USE
+
 echo "Installing packages into ${ROOT}..."
 INSTALL_MASK="${clst_install_mask}" LC_CTYPE=C.utf8 \
 	run_merge --oneshot "${clst_embedded_packages}"
