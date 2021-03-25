fd8cdc29f8fc9e026279052d068e37f7ea3799c0
Daniel Cordero <catalyst@0xdc.io>
2021-03-25 03:07:53 +0000

    stage4: build packages with C.utf8 Ctype
    Some python packages require a utf8 ctype when opening files during
    compilation. While these packages are not required for stage123,
    there are a handful which might be pulled in as additional packages
    in stage4/embedded.
    
    Until a more permanent fix is in place, set LC_CTYPE to a utf8 value.
    
    https://bugs.gentoo.org/688330
    https://devmanual.gentoo.org/eclass-reference/python-utils-r1.eclass/index.html#lbAE

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -3,10 +3,11 @@
 source /tmp/chroot-functions.sh
 
 echo "Installing dependencies..."
-ROOT=/ run_merge -o "${clst_embedded_packages}"
+ROOT=/ run_merge --update --onlydeps "${clst_embedded_packages}"
 
+echo "Installing packages into ${clst_root_path}..."
 export ROOT="${clst_root_path}"
 mkdir -p "$ROOT"
 
-INSTALL_MASK="${clst_install_mask}" \
-	run_merge -1 -O "${clst_embedded_packages}"
+INSTALL_MASK="${clst_install_mask}" LC_CTYPE=C.utf8 \
+	run_merge --oneshot "${clst_embedded_packages}"
