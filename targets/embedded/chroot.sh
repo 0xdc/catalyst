f857e30977955a406d0392da2fdc4214b43d682d
Daniel Cordero <catalyst@0xdc.io>
2021-06-11 18:51:11 +0000

    patch embedded-splitusr

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -13,14 +13,20 @@ export ROOT="${clst_root_path}"
 echo "Installing base system into ${ROOT}..."
 run_merge sys-apps/baselayout
 
+split_usr
+
 export USE="-* build systemd udev"
 run_merge sys-libs/glibc sys-apps/systemd
 run_merge app-shells/bash
 unset USE
+
+split_usr
 
 echo "Installing packages into ${clst_root_path}..."
 
 for package in ${clst_embedded_packages}; do
 	INSTALL_MASK="${clst_install_mask}" LC_CTYPE=C.utf8 \
 		run_merge "${package}"
+
+	split_usr
 done
