6fe676f4b8eb4f45426d71f9b0360d570b124834
Daniel Cordero <catalyst@0xdc.io>
2021-12-14 14:26:24 +0000

    stage4: bring world up-to-date
    Changes in package.use aren't picked up in packages that are not in system.

--- targets/stage4/chroot.sh
+++ targets/stage4/chroot.sh
@@ -2,8 +2,8 @@
 
 source /tmp/chroot-functions.sh
 
-echo "Bringing system up to date using profile specific use flags"
-run_merge -u @system
+echo "Bringing world up to date using profile specific use flags"
+run_merge --update --deep --newuse @world
 
 echo "Emerging packages using stage4 use flags"
 
