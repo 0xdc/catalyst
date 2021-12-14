1bc3fcf8ccc52d620736d46f74e72013a6b06499
Daniel Cordero <catalyst@0xdc.io>
2021-12-14 14:39:06 +0000

    patch stage3-harfbuzz-freetype

--- targets/stage3/chroot.sh
+++ targets/stage3/chroot.sh
@@ -6,7 +6,11 @@ export CONFIG_PROTECT="-* /etc/locale.gen"
 
 echo "$locales" > /etc/locale.gen
 
-run_merge -e --update --deep --with-bdeps=y @system
+run_merge -e --update --deep --with-bdeps=y @system || {
+	USE=-harfbuzz run_merge -1 freetype
+	run_merge -1 harfbuzz
+	run_merge -e --update --deep --with-bdeps=y @system
+}
 
 # Replace modified /etc/locale.gen with default
 etc-update --automode -5
