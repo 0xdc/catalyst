186f941a214768791617129cf74e90a93c789791
Daniel Cordero <catalyst@0xdc.io>
2022-04-19 12:44:36 +0000

    Use a 3 stage livecd build
    The current (2 stage) livecd process requires a full stage3/4 tarball
    that can install genkernel and compile a kernel for the livecd. Once the
    kernel is built, packages are then unmerged to reduce the size of the CD
    image. This means explicity removing every unnecessary package that may
    be installed from the system set, or additional stage4 world sets.
    
    Instead, an embedded target could be used, or a stage1 target be
    abused, to create minimal images for the livecd environment.
    
    This patch moves some actions from livecd-stage1 and livecd-stage2 into
    a new target: livecd-stage3.
    
    livecd-stage1 is now very similar to a regular stage4, where additional
    packages can be installed through the package manager. This step would
    be optional for embedded stages without a package manager.
    
    livecd-stage2 now uses a stage3 to build a kernel and reusable "cdtar".
    The seed stage is not retained.
    
    livecd-stage3 takes the built kernel and modules, and a
    livecd-stage1/embedded/stage3/stage4 tarball. Without package manager
    interaction, it combines these into a livecd. Modules for the built
    kernels are extracted into the looped image.
    
    This patch is an RFC as it will break a lot of existing specfiles, and a
    fsscript would also be necessary for the resulting livecd to be useful.
    
    The code basically copies the existing livecd-stage1/livecd-stage2 code
    in the livecd-stage3 target and support scripts. I'm sure this could be
    cleaned up to remove the references in livecd-stage1.py/livecd-stage2.py
    
    The livecd-stage3 specfile requires the 'profile:' keyword, even though
    it is not required. This is because it is defined as a requirement by
    StageBase.__init__()

---
+++ catalyst/targets/livecd_stage3.py
@@ -0,0 +1,61 @@
+"""
+LiveCD stage3 target
+"""
+# NOTE: That^^ docstring has influence catalyst-spec(5) man page generation.
+
+from catalyst.support import normpath
+from catalyst.fileops import clear_dir
+from catalyst.targets.livecd_stage1 import livecd_stage1
+
+
+class livecd_stage3(livecd_stage1):
+    """
+    Builder class for LiveCD stage3.
+
+    Wraps any stage into a CD image
+
+    Requires a cached kernel name and cdtar
+    """
+    required_values = frozenset([
+        "boot/kernel",
+        "livecd/cdtar",
+    ])
+    valid_values = required_values | frozenset([
+        "livecd/bootargs",
+        "livecd/empty",
+        "livecd/fsscript",
+        "livecd/fstype",
+        "livecd/iso",
+        "livecd/rm",
+        "livecd/root_overlay",
+        "livecd/type",
+        "livecd/verify",
+        "livecd/volid",
+    ])
+
+    def set_action_sequence(self):
+        self.settings['action_sequence'] = [
+            "unpack",
+            "bind",
+            "setup_environment",
+            "root_overlay",
+            "fsscript",
+            "bootloader",
+            "unbind",
+            "remove",
+            "empty",
+            "target_setup",
+            "create_iso",
+            "clear_autoresume",
+            "remove_chroot",
+        ]
+
+    def set_target_path(self):
+        '''Set the target path for the finished stage.
+
+        This method runs the StageBase.set_target_path method,
+        and additionally creates a staging directory for assembling
+        the final components needed to produce the iso image.
+        '''
+        super(livecd_stage1, self).set_target_path()
+        clear_dir(self.settings['target_path'])
