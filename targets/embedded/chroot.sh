b9199da347c66db8c86149b02710fdf7d2458a28
Daniel Cordero <gentoo.catalyst@0xdc.io>
2022-04-20 10:28:14 +0000

    patch embedded-expand-args

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -3,10 +3,10 @@
 source /tmp/chroot-functions.sh
 
 echo "Installing dependencies..."
-ROOT=/ run_merge -o "${clst_embedded_packages}"
+ROOT=/ run_merge --update --onlydeps "${clst_embedded_packages}"
 
 export ROOT="${clst_root_path}"
 mkdir -p "$ROOT"
 
 INSTALL_MASK="${clst_install_mask}" \
-	run_merge -1 -O "${clst_embedded_packages}"
+	run_merge --oneshot "${clst_embedded_packages}"
