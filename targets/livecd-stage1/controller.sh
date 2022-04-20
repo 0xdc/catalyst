ed6674cfa6d5b42d16cdd19bd1e61e8e5bd4b337
Daniel Cordero <catalyst@0xdc.io>
2022-04-20 10:28:14 +0000

    patch livecd1-unmerge

--- targets/livecd-stage1/controller.sh
+++ targets/livecd-stage1/controller.sh
@@ -12,5 +12,12 @@ case $1 in
 		exec_in_chroot \
 			${clst_shdir}/${clst_target}/chroot.sh
 		;;
+
+	unmerge)
+		shift
+		export clst_packages="$*"
+		exec_in_chroot ${clst_shdir}/support/unmerge.sh
+		[ "${clst_livecd_depclean}" != "no" ] && exec_in_chroot ${clst_shdir}/support/depclean.sh
+	;;
 esac
 exit $?
