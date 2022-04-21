a23dbb7320f03d36b804b4cc98fcd82ab3b7e9df
Daniel Cordero <gentoo.catalyst@0xdc.io>
2022-04-21 14:49:57 +0000

    Create a dracut initrd
    Stop using genkernel's initrd. After the kernel and modules are built
    append the initrd to the kerncache output for unpacking later.
    
    Modify the bootloader configuration to use dracut command lines.
    
    Remove unsupported bootloader arches

--- targets/systemd/controller.sh
+++ targets/systemd/controller.sh
@@ -12,22 +12,19 @@ case ${1} in
 
 	pre-kmerge)
 		# Sets up the build environment before any kernels are compiled
-		exec_in_chroot ${clst_shdir}/support/pre-kmerge.sh
+		exec_in_chroot ${clst_shdir}/${clst_target}/pre-kmerge.sh
 	;;
 
 	kernel)
 		shift
 		export kname="${1}"
 
-		exec_in_chroot ${clst_shdir}/support/kmerge.sh
+		exec_in_chroot ${clst_shdir}/${clst_target}/kmerge.sh
 	;;
 
 	bootloader)
 		shift
-		# Here is where we poke in our identifier
-		touch ${1}/livecd
-
-		${clst_shdir}/support/bootloader-setup.sh ${1}
+		${clst_shdir}/${clst_target}/bootloader-setup.sh ${1}
 
 		for x in ${clst_boot_kernel}; do
 			extract_modules "${clst_chroot_path}/${clst_root_path}" $x
---
+++ targets/systemd/pre-kmerge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RUN_DEFAULT_FUNCS="yes"
+
+source /tmp/chroot-functions.sh
+
+if [[ ${clst_hostarch} == hppa ]]; then
+	for i in ${clst_boot_kernel}; do
+		case ${i} in
+			*32)
+				let num32++
+				;;
+			*64)
+				let num64++
+				;;
+			*)
+				die "Kernel names must end with either \"32\" or \"64\""
+				;;
+		esac
+	done
+	[[ $num32 > 1 ]] && die "Only one 32-bit kernel can be configured"
+	[[ $num64 > 1 ]] && die "Only one 64-bit kernel can be configured"
+fi
+
+run_merge --oneshot --update sys-kernel/genkernel sys-kernel/dracut sys-fs/lvm2
+install -d /tmp/kerncache
