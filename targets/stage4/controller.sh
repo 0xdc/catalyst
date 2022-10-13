fc0e82918223933f8d5c02c8c14ad492ee5e3889
Daniel Cordero <catalyst@0xdc.io>
2022-10-13 16:47:10 +0000

    patch stage4-depclean

--- targets/stage4/controller.sh
+++ targets/stage4/controller.sh
@@ -65,6 +65,7 @@ case $1 in
 		shift
 		export clst_packages="$*"
 		exec_in_chroot ${clst_shdir}/support/unmerge.sh
+		[ "${clst_stage4_depclean}" != "no" ] && exec_in_chroot ${clst_shdir}/support/depclean.sh
 	;;
 
 	iso)
