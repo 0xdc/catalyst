6b499de36aaa5d205d05701ce42cadc2f532f16d
Daniel Cordero <catalyst@0xdc.io>
2023-01-02 15:51:29 +0000

    Allow the use of mksquashfs to pack the livecd contents

--- targets/support/target_image_setup.sh
+++ targets/support/target_image_setup.sh
@@ -7,8 +7,13 @@ mkdir -p "${1}"
 echo "Creating ${clst_fstype} filesystem"
 case ${clst_fstype} in
 	squashfs)
-		gensquashfs -k -D "${clst_stage_path}" -q ${clst_fsops} "${1}/image.squashfs" \
-			|| die "Failed to create squashfs filesystem"
+		if command -v mksquashfs; then
+			mksquashfs "${clst_stage_path}" "{1}/image.squashfs" "${clst_fsops}" -noappend \
+				|| die "mksquashfs failed"
+		else
+			gensquashfs -k -D "${clst_stage_path}" -q ${clst_fsops} "${1}/image.squashfs" \
+				|| die "Failed to create squashfs filesystem"
+		fi
 	;;
 	jffs2)
 		mkfs.jffs2 --root="${clst_stage_path}" --output="${1}/image.jffs" "${clst_fsops}" \
