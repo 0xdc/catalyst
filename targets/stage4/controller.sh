85525dfa740a80a76d5dd7372055ac1a1c3c4e27
Daniel Cordero <gentoo.catalyst@0xdc.io>
2023-03-05 22:02:58 +0000

    patch depclean

--- targets/stage4/controller.sh
+++ targets/stage4/controller.sh
@@ -57,7 +57,7 @@ case $1 in
 		shift
 		export clst_packages="$*"
 		exec_in_chroot ${clst_shdir}/support/unmerge.sh
-		[ "${clst_stage4_depclean}" != "no" ] && exec_in_chroot ${clst_shdir}/support/depclean.sh
+		exec_in_chroot ${clst_shdir}/support/depclean.sh
 	;;
 
 	iso)
