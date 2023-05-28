675c1e3cee854ec0b75c7889e7cb1110762f3da5
Daniel Cordero <gentoo.catalyst@0xdc.io>
2023-05-28 09:04:07 +0000

    patch stage1-splitusr
    patch embedded-splitusr

--- targets/embedded/chroot.sh
+++ targets/embedded/chroot.sh
@@ -13,13 +13,20 @@ export ROOT="${clst_root_path}"
 echo "Installing base system into ${ROOT}..."
 run_merge sys-apps/baselayout
 
+split_usr
+
 export USE="-* build systemd udev gawk pigz"
+
 run_merge sys-libs/glibc sys-apps/systemd
 run_merge app-shells/bash
 unset USE
+
+split_usr
 
 echo "Installing packages into ${clst_root_path}..."
 for package in ${clst_embedded_packages}; do
 	INSTALL_MASK="${clst_install_mask}" \
 		run_merge "${package}"
+
+	split_usr
 done
