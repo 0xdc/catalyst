8118d91a895dc3d3dceb932199ccc104a59ed816
Daniel Cordero <catalyst@0xdc.io>
2023-12-29 19:07:54 +0000

    stage3: run curl fixup on gnome profiles

--- targets/stage3/chroot.sh
+++ targets/stage3/chroot.sh
@@ -11,6 +11,9 @@ case "${clst_profile}" in
 	USE=-harfbuzz run_merge --oneshot --update media-libs/freetype
 	USE=-cairo run_merge --oneshot --update media-libs/harfbuzz
 	;;
+*gnome*)
+	USE=-http2 run_merge --oneshot --update net-misc/curl
+	;;
 esac
 run_merge -e --update --deep --with-bdeps=y @system
 
